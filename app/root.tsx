b108c9545961a5dfc695f8e99fda4fab8ccfc878
Ricky <drickyriverab@gmail.com>
2022-03-16 20:58:10 +0100

    hi

--- app/root.tsx
+++ app/root.tsx
@@ -21,13 +21,20 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body  style={{ backgroundColor:"transparent" }}>
+      <body  style={{ backgroundColor:"transparent"}}>
+        <div style={{ display: "flex" }}>
+        <div style={{ width: "50vw"}}>
         <iframe
           src="https://minnit.chat/rickychat?embed&&nickname="
           style={{ border: "none", width: "100%", height: "500px"}}
           allowTransparency={true}
         ></iframe>
         <br />
+        </div>
+        <div>
+          <img style={{ height: "500px", width: "100%" }} src="https://res.cloudinary.com/do02qffq1/image/upload/v1647460280/samples/ezgif.com-gif-maker_1_nibhbq.gif" alt="" />
+        </div>
+        </div>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
